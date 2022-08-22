81f56b81f27975bc8a95243f132209cd2d6bd752
Kati Perkiö <kati.perkio@metropolia.fi>
2022-08-22 14:38:56 +0300

    Homework commit

---
+++ 2.4.py
@@ -0,0 +1,11 @@
+ekaluku = float(input("Anna ensimmäinen kokonaisluku: "))
+tokaluku = float(input("Anna toinen kokonaisluku: "))
+kolmasluku = float(input("Anna kolmas kokonaisluku: "))
+
+summa = ekaluku + tokaluku + kolmasluku
+tulo = ekaluku * tokaluku * kolmasluku
+keskiarvo = summa / 3
+
+print(f"Lukujen summa on: {summa}")
+print(f"Lukujen tulo on: {tulo}")
+print(f"Lukujen keskiarvo on {keskiarvo}")
