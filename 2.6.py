86cb8b9fcea817ce763d269409ff40e77cd1b139
Kati Perkiö <kati.perkio@metropolia.fi>
2022-08-25 13:41:40 +0300

    Homework commit

---
+++ 2.6.py
@@ -0,0 +1,10 @@
+import random
+
+print(random.randint(0,9), end = '')
+print(random.randint(0,9), end = '')
+print(random.randint(0,9))
+
+print(random.randint(1,6), end = '')
+print(random.randint(1,6), end = '')
+print(random.randint(1,6), end = '')
+print(random.randint(1,6))
