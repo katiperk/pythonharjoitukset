b67fa17be07a96fb851e80e180a711ce33e085f9
Kati Perkiö <kati.perkio@metropolia.fi>
2022-09-07 13:47:28 +0300

    Funktiotreeni

---
+++ funktio_1.py
@@ -0,0 +1,11 @@
+def tervehdi():
+    print("Moi!")
+    print("Tämä on tervehdys funktiosta!")
+    return
+
+print("Ollaan pääohjelmassa.")
+tervehdi()
+print("Ollaan takaisin pääohjelmassa.")
+tervehdi()
+tervehdi()
+print("No nyt lopulta lopetellaan")
