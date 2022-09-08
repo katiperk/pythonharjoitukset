b326fbc47db4920c11160d79f0e603d0a577cf57
Kati Perkiö <kati.perkio@metropolia.fi>
2022-09-08 10:45:33 +0300

    Homework commit

--- 6.5.py
+++ 6.5.py
@@ -1,8 +1,8 @@
 def parittomatPois(luvut):
-    if (koko == 0):
-        return 0
-    else:
-        return luvut[koko - 1] + summaus(luvut, koko - 1)
+    for i in luvut:
+        if i % 2 != 0:
+            luvut.remove(i)
+    return luvut
 
 luvut = []
 
@@ -12,5 +12,6 @@ for i in range(0, koko):
     luku = int(input())
     luvut.append(luku)
 
-summa = summaus(luvut, koko)
-print(f"Listan lukujen summa: {summa}")
+print(luvut)
+paritonLista = parittomatPois(luvut)
+print(paritonLista)
