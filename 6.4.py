a19e145156d7359a7ad6cc474554b5e1188ff7cb
Kati Perkiö <kati.perkio@metropolia.fi>
2022-09-08 10:34:42 +0300

    Homework commit

---
+++ 6.4.py
@@ -0,0 +1,16 @@
+def summaus(numerot, koko):
+    if (koko == 0):
+        return 0
+    else:
+        return luvut[koko - 1] + summaus(luvut, koko - 1)
+
+luvut = []
+
+koko = int(input("Anna listan koko: "))
+
+for i in range(0, koko):
+    luku = int(input())
+    luvut.append(luku)
+
+summa = summaus(luvut, koko)
+print(f"Listan lukujen summa: {summa}")
