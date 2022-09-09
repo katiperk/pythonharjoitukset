e134be05472811453a298c7739e820053a4a5d06
Kati Perkiö <kati.perkio@metropolia.fi>
2022-09-09 12:23:29 +0300

    Homework commit

--- 3.2.py
+++ 3.2.py
@@ -1,12 +1,12 @@
 hyttiluokka = input("Kerro laivan hyttiluokka: ")
 
-if hyttiluokka == 'LUX':
+if hyttiluokka == 'LUX' or hyttiluokka == 'lux':
     print("LUX on parvekkeellinen hytti yläkannella.")
-elif hyttiluokka == 'A':
+elif hyttiluokka == 'A' or hyttiluokka == 'a':
     print("A on ikkunallinen hytti autokannen yläpuolella.")
-elif hyttiluokka == 'B':
+elif hyttiluokka == 'B' or hyttiluokka == 'b':
     print("B on ikkunaton hytti autokannen yläpuolella.")
-elif hyttiluokka == 'C':
+elif hyttiluokka == 'C' or hyttiluokka == 'c':
     print("C on ikkunaton hytti autokannen alapuolella.")
 else:
     print("Virheellinen hyttiluokka.")
