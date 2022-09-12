bc5d13f235499d7290ef7dade8619be16039b2c5
Kati Perkiö <kati.perkio@metropolia.fi>
2022-09-12 09:16:18 +0300

    Homework commit

--- 4.2.py
+++ 4.2.py
@@ -1,7 +1,10 @@
+tuumat = 0
+# En saanut ohjelmaa toimimaan niin, että negatiivinen tuumamäärä jäisi tulostamatta. Ohjelma kuitenkin pysähtyy, joten sinällään toimii.
+
 while True:
-    tuumat = float(input("Anna tuumat: "))
-    if tuumat >= 0:
+    if tuumat < 0:
+        break
+    elif tuumat >= 0:
+        tuumat = float(input("Anna tuumat: "))
         senttimetrit = tuumat * 2.51
         print(f"Senttimetreinä: {senttimetrit:.2f}")
-    else:
-        break
