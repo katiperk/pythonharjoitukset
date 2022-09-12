ed48537e72cc0ab0c946a5d2bfaef0ff60c7d1a3
Kati Perkiö <kati.perkio@metropolia.fi>
2022-09-12 10:27:08 +0300

    Homework commit

---
+++ 4.4.py
@@ -0,0 +1,13 @@
+import random
+
+luku = random.randint(1, 10)
+
+while True:
+    arvaus = int(input("Arvaa luku väliltä 1-10! "))
+    if arvaus < luku:
+        print("Liian pieni arvaus")
+    elif arvaus > luku:
+        print("Liian suuri arvaus")
+    elif arvaus == luku:
+        print("Oikein!")
+        break
